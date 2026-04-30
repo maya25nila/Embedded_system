89fca85286ca0e4c0f8abea00ec23013fb61b74c
Maya <maya25nila@gmail.com>
2026-04-30 15:35:19 +0530

    Removed folder

--- STM_32/Driver_dev/Debug/Drivers/src/stm32f401xx_gpio_driver.d
+++
@@ -1,6 +0,0 @@
-Drivers/src/stm32f401xx_gpio_driver.o: \
- ../Drivers/src/stm32f401xx_gpio_driver.c \
- C:/Users/littles/Desktop/embedded\ system/STM_32/Driver_dev/Drivers/inc/stm32f401xx_gpio_driver.h \
- C:/Users/littles/Desktop/embedded\ system/STM_32/Driver_dev/Drivers/inc/stm32f401xx.h
-C:/Users/littles/Desktop/embedded\ system/STM_32/Driver_dev/Drivers/inc/stm32f401xx_gpio_driver.h:
-C:/Users/littles/Desktop/embedded\ system/STM_32/Driver_dev/Drivers/inc/stm32f401xx.h:
